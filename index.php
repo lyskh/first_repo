3d5ee3c5b3aa503dccbe9ff280b7aa7cc01efa63
lyskh <allysakhaerepeso@csab.edu.ph>
2024-01-26 22:13:23 +0800

    first repository activity

---
+++ index.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+</head>
+
+<body class="bg-light">
+
+
+    <h1 class="mt-5 text-center">First Repository Commit</h1>
+
+    <div class="container">
+    <form>
+        <div class="row">
+       
+            <div class="col-lg-3"></div>
+            <div class="col-lg-6 mt-5">
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Firstname:</label>
+                    <input name="firstname" type="text" class="form-control" />
+                </div>
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Lastname:</label>
+                    <input name="lastname" type="text" class="form-control" />
+                </div>
+
+                <div class="mb-3">
+                    <button type="submit" class="btn btn-dark shadow-none">Submit Name</button>
+                </div>
+
+            </div>
+           
+        </div>
+        </form>
+
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js">
+    </script>
+    
+</body>
+
+</html>
